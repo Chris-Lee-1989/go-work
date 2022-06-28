30607ade0b6187cebb8a82ba61283ce9716b734c
이윤민 <leeyoonmin@iyunmin-ui-iMac.local>
2022-06-28 18:30:33 +0900

    MOD 운영 api 주소 뼌경

--- src/modules/apiUrl.tsx
+++ src/modules/apiUrl.tsx
@@ -2,11 +2,11 @@ import {Platform} from 'react-native';
 
 export default (() => {
   if (Platform.OS === 'android') {
-    return 'http://192.168.219.105:8080';
+    // return 'http://192.168.219.105:8080';
     // return 'http://10.0.2.2:8080';
-    // return 'https://wms-api.flowerfarm.co.kr';
+    return 'https://wms-api.flowerfarm.co.kr';
   } else {
-    return 'http://192.168.219.105:8080';
-    // return 'https://wms-api.flowerfarm.co.kr';
+    // return 'http://192.168.219.105:8080';
+    return 'https://wms-api.flowerfarm.co.kr';
   }
 })();
